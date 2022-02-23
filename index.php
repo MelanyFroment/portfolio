0dafffc677951ed4a2d82c0d0a6d020cb0c7a75a
melany froment <melany.from@yahoo.com>
2022-02-23 14:46:18 +0100

    html to php corriger

--- index.php
+++ index.php
@@ -605,7 +605,7 @@
 									</div>
 								</div>
 								<div class="col-sm-6">
-									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92372.03854231324!2d6.866962072639001!3d43.65594476892793!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12cc25fe3958edfb%3A0x117727988cf82e2d!2sGrasse!5e0!3m2!1sfr!2sfr!4v1641319560933!5m2!1sfr!2sfr" width="600" height="650" style="border:0;" allowfullscreen="" loading="lazy" class="img-fluid"></iframe>
+									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92372.03854231324!2d6.866962072639001!3d43.65594476892793!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12cc25fe3958edfb%3A0x117727988cf82e2d!2sGrasse!5e0!3m2!1sfr!2sfr!4v1641319560933!5m2!1sfr!2sfr" width="600" height="650" style="border:0;" allowfullscreen="" loading="lazy" class="img-fluid carte"></iframe>
 								</div>
 							</div>
 						</div>
