8b06e6101cc4594f8de69b42335103d073179368
melany froment <melany.from@yahoo.com>
2022-02-23 14:34:02 +0100

    html to php corriger

--- index.php
+++ index.php
@@ -571,42 +571,6 @@
 					</div>
 					<embed src="./assets/img/Tableau-synthese.pdf" width=100% height=500 type='application/pdf'/>
 				</div>
-				
-				
-				
-				
-				<!--<div class="row">
-					<div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
-						<div class="icon"><i class="bi bi-briefcase"></i></div>
-						<h4 class="title"><a href="">Lorem Ipsum</a></h4>
-						<p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
-					</div>
-					<div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
-						<div class="icon"><i class="bi bi-card-checklist"></i></div>
-						<h4 class="title"><a href="">Dolor Sitema</a></h4>
-						<p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
-					</div>
-					<div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
-						<div class="icon"><i class="bi bi-bar-chart"></i></div>
-						<h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-						<p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-					</div>
-					<div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
-						<div class="icon"><i class="bi bi-binoculars"></i></div>
-						<h4 class="title"><a href="">Magni Dolores</a></h4>
-						<p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-					</div>
-					<div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
-						<div class="icon"><i class="bi bi-brightness-high"></i></div>
-						<h4 class="title"><a href="">Nemo Enim</a></h4>
-						<p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
-					</div>
-					<div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
-						<div class="icon"><i class="bi bi-calendar4-week"></i></div>
-						<h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-						<p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
-					</div>
-				</div>-->
 			</div>
 		</section>
 
@@ -618,7 +582,7 @@
 					<h2>Contact</h2>
 				</div>
 				<div class="row" data-aos="fade-in">
-					<div class="col-lg-5 d-flex align-items-stretch">
+					<div class="col-lg-12 d-flex align-items-stretch">
 						<div class="info">
 							<div class="address">
 								<i class="bi bi-geo-alt"></i>
@@ -640,7 +604,7 @@
 							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92372.03854231324!2d6.866962072639001!3d43.65594476892793!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12cc25fe3958edfb%3A0x117727988cf82e2d!2sGrasse!5e0!3m2!1sfr!2sfr!4v1641319560933!5m2!1sfr!2sfr" width="600" height="650" style="border:0;" allowfullscreen="" loading="lazy" class="img-fluid"></iframe>
 						</div>
 					</div>
-					<div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+					<!-- <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
 						<form action="forms/contact.php" method="post" role="form" class="php-email-form">
 							<div class="row">
 								<div class="form-group col-md-6">
@@ -667,7 +631,7 @@
 							</div>
 							<div class="text-center"><button type="submit">Message envoyé</button></div>
 						</form>
-					</div>
+					</div> -->
 				</div>
 			</div>
 		</section>
