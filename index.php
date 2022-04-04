a07301a476f404f0992ff7f0d3084324a650578f
melany froment <melany.from@yahoo.com>
2022-04-04 19:51:41 +0200

    Modif et completementation

--- index.php
+++ index.php
@@ -46,6 +46,7 @@
 						<li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>BTS SIO</span></a></li>
 						<li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Projet</span></a></li>
 						<li><a href="#services" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Veille informatique</span></a></li>
+						<li><a href="#tableau" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Veille Juridique</span></a></li>
 						<li><a href="#services1" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Veille Juridique</span></a></li>
 						<li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
 					</ul>
@@ -523,7 +524,7 @@
 							</ul>
 						</ul>
 					</p> 
-					<div class="section-title mt-5">
+					<div id="tableau" class="section-title mt-5">
 						<h2>Tableau de synthése</h2> 
 					</div>
 					<embed src="./assets/img/Tableau-synthese.pdf" width=100% height=500 type='application/pdf'/>
